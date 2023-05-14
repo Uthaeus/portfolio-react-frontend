66687a466b9a4f7a8eda233cc2684ac0bd0044b3
Uthaeus <romanlavery@gmail.com>
2023-05-14 17:25:34 -0600

    added delete button and function for category item

--- src/components/category/category-item.js
+++ src/components/category/category-item.js
@@ -1,11 +1,39 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+
+import { UserContext } from "../store/user-context";
 
 function CategoryItem({ category }) {
+    const { user } = useContext(UserContext);
+
+    function itemDeleteHandler() {
+        console.log('Delete Category');
+
+        fetch(`http://localhost:4000/categories/${category.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('portfolio_token'),
+            }
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .catch(error => console.log('category delete error: ', error));
+    }
+
     return (
-        <Link to='#' className="category-item">
-            <h3>{category.name}</h3>
-            <p>{category.description}</p>
-        </Link>
+        <div className="category-item-wrapper">
+            <Link to='#' className="category-item">
+                <h3>{category.title}</h3>
+            </Link>
+            {user && user.role === 'site_admin' && (
+                <div className="category-item-actions">
+                    <button onClick={itemDeleteHandler} className="btn btn-danger">Delete</button>
+                </div>
+            )}
+        </div>
     )
 }
 
